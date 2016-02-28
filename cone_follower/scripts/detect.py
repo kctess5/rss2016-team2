2ca392a5b15f43e6112a21d0266504c349ade7e3
Jessica Kenney <jessk@mit.edu>
2016-02-28 16:10:20 -0500

    forward image across channels

--- cone_follower/scripts/detect.py
+++ cone_follower/scripts/detect.py
@@ -2,13 +2,18 @@
 import rospy
 from sensor_msgs.msg import Image
 
-def detect():
-	rospy.init_node('detect', anonymous=True)
-	print "Hello!"
-	rospy.spin()
+class ConeDetector(object):
+	def __init__(self):
+		rospy.init_node('detect', anonymous=True)
+		self.sub = rospy.Subscriber('/camera/rgb/image_rect_color', Image, self.recv_image)
+		self.pub = rospy.Publisher('/cone', Image)
+
+		rospy.spin()
+	def recv_image(self, img):
+		self.pub.publish(img)
 
 if __name__ == '__main__':
 	try:
-		detect()
+		ConeDetector()
 	except rospy.ROSInterruptException:
 		pass
