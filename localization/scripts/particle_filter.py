010a9768022d768ca4e68546e7229dc7a72d61c5
Miles Steele <mlsteele@mit.edu>
2016-03-13 15:14:22 -0400

    fix up a teeny bit (sorry my editor kills trailing spaces)

--- localization/scripts/particle_filter.py
+++ localization/scripts/particle_filter.py
@@ -14,6 +14,7 @@ from scipy import signal
 import math
 import numpy as np
 import random
+import collections
 
 """
 Localizes Stuff.
@@ -21,29 +22,29 @@ Localizes Stuff.
 class Localizer(object):
     def __init__(self):
         rospy.loginfo("Initializing Monte Carlo Localization particle filter")
-        
+
         rospy.init_node('localizer', anonymous=True)
         LASER_SCAN_TOPIC = "/scan"
         self.sub = rospy.Subscriber(LASER_SCAN_TOPIC, numpy_msg(LaserScan), self.scan_callback)
         self.odom = rospy.Subscriber("/vesc/odom", Odometry, self.odometry_callback)
         self.pub = rospy.Publisher('/cone/key_points', PolarPoints, queue_size=10)
-        
+
         # 2D numpy array of occupancy floats in [0,1].
         self.omap = self.get_omap()
-        
+
         # accumulated odometry delta - reset whenever the particle filter runs
         self.clear_odometry() # Delta(0,0,0)
         self.last_pose = None
         self.last_scan = None
-        
+
         # container for the persistent particles
         self.particles = []
-        
+
         # load all configuration variables
         self.configure()
-        
+
         rospy.Timer(rospy.Duration(1.0 / self.LOCALIZATION_FREQUENCY), self.timer_callback)
-        
+
     def configure(self):
         # the amount of noise to add for each component of the particle state
         # increasing this variable will make the particles diverge faster
@@ -53,20 +54,20 @@ class Localizer(object):
         self.LOCALIZATION_FREQUENCY = 15.0
         # TODO - better initial pose management
         self.INITIAL_POSE = Particle(0,0,0)
-        
+
     def scan_callback(self, data):
         rospy.logdebug("Storing scan data")
         self.last_scan = data
-        
+
     def odometry_callback(self, data):
         rospy.logdebug("Storing odometry data")
         if self.last_pose == None:
             self.last_pose = data.pose.pose
             return
-            
+
         pos = data.pose.pose.position
         orientation = data.pose.pose.orientation
-        
+
         # calculate delta between this and last sensor reading
         x_d = pos.x - self.last_pose.position.x
         y_d = pos.y - self.last_pose.position.y
@@ -75,22 +76,21 @@ class Localizer(object):
         # store deltas
         aod = self.accumulated_odometry_delta
         self.accumulated_odometry_delta = (x_d + aod.x, y_d + aod.y, heading_d + aod.heading)
-        
+
         # store this pose message for future use
         self.last_pose = data.pose.pose
-        
+
     def clear_odometry(self):
         rospy.logdebug("Clearing accumulated odometry")
         self.accumulated_odometry_delta = Delta(0,0,0)
-         
+
     def get_omap(self):
         """Get the map from the map service and return it as a numpy array.
         This blocks until the map service is available.
         """
-        map_service_name = rospy.getparam("~static_map", "static_map")
+        map_service_name = rospy.get_param("~static_map", "static_map")
         rospy.wait_for_service(map_service_name)
         map_msg = rospy.ServiceProxy(map_service_name, GetMap)()
-        rospy.wait_for_service('add_two_ints')
         width, height = map_msg.info.width, map_msg.info.height
         array_255 = map_msg.data.reshape((height, width))
         array_float = array_255.astype(float) / 255.
@@ -100,7 +100,7 @@ class Localizer(object):
         return Particle(particle.x + delta.x + self.RANDOMNESS.x * np.random.normal(), \
             particle.y + delta.y + self.RANDOMNESS.y * np.random.normal(), \
             particle.heading + delta.heading + self.RANDOMNESS.heading * np.random.normal())
-    
+
     def sensor_update(self, omap, measurements, particles):
         """Calculate weights for particles given a map and sensor data.
         Basically the likelihood of the measurement at the location.
@@ -112,7 +112,7 @@ class Localizer(object):
             An array of weights for the particles.
         """
         return [self.sensor_update_individual(omap, sensors, particle) for particle in particles]
-       
+
     def sensor_update_individual(self, omap, measurement, particle):
         """Whee, scan matching."""
         # Angles in the scan relative to the robot.
@@ -125,22 +125,22 @@ class Localizer(object):
                            for a in measurement_ranges]
         weight = ((expected_ranges - measurement.ranges) ** 2).mean()
         return weight
-    
+
     def calc_range(self, omap, x, y, heading, max_range):
         """
         Calculate the expected laser reading at a given point and angle.
         Do this by walking along the ray until you reach something that the map thinks is filled.
-        
+
         Copied from https://bitbucket.org/alexbuyval/ardroneum
         """
         robot_x, robot_y, robot_a = x, y, heading
-        
+
         def is_valid(y, x):
             return (0 <= y < omap.shape[0] and
                     0 <= x < omap.shape[1])
         # What is scale?
         scale = 1
-        
+
         x0, y0 = robot_x, robot_y
         x1, y1 = robot_x + max_range*math.cos(robot_a), robot_y + max_range*math.sin(robot_a)
 
@@ -189,51 +189,51 @@ class Localizer(object):
                         return (math.sqrt((x-x0)*(x-x0) + (y-y0)*(y-y0)) / scale)
 
         return max_range
-        
+
     def init_particle(self):
         rospy.logdebug("Initializing particle at: (%.2f, %.2f, %.2f)", \
             self.INITIAL_POSE.x, self.INITIAL_POSE.y, self.INITIAL_POSE.heading)
-        
+
         self.particles.append(self.INITIAL_POSE)
-      
+
     def timer_callback(self, event):
         # initialize particles if necessary
         while len(self.particles) < self.NUM_PARTICLES:
             self.init_particle()
-            
+
         # don't start until scanner data is available
         if self.last_scan == None:
             return
 
         self.particles = \
             self.MCL(self.omap, self.particles, self.accumulated_odometry_delta, self.last_scan)
-        
+
     def MCL(self, omap, previous_particles, odometry_delta, sensors):
         rospy.logdebug("Attempting Monte Carlo Localization")
-        
+
         # update particle positions based on odometry readings
         particles = \
             map(lambda old_particle: self.motion_update(odometry_delta, old_particle), \
                 previous_particles)
-        
+
         # reset accumulated odometry
         self.clear_odometry()
-        
+
         # update particle weights according to probability of recording the given sensor readings
         particle_weights = \
             map(lambda old_particle: self.sensor_update(omap, sensors, old_particle), previous_particles)
 
         # compute sum of all particle weights
         particle_mass = sum(particle_weights)
-        
+
         # reweight particles - normalization of probability
         particle_weights = map(lambda x: x / particle_mass, particle_weights)
-        
+
         # fill new_particles by sampling from the reweighted particle array with replacement
         new_particles = np.random.choice(particles, len(particles), True, particle_weights)
 
         return new_particles
-        
+
 Particle = collections.namedtuple("Particle", ["x", "y", "heading"])
 Delta = collections.namedtuple("Delta", ["x", "y", "heading"])
 
