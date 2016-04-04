19871ca8cd0c228c07e680912328135011893269
chwalsh <chwalsh@mit.edu>
2016-04-04 16:43:05 -0400

    more code

--- local_costmap/scripts/local_costmap.py
+++ local_costmap/scripts/local_costmap.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import rospy
 from scipy import ndimage
+from nav_msgs import OccupancyGrid
 
 from visualization_driver import VisualizationDriver
 
@@ -50,6 +51,79 @@ class FrameBuffer:
             return self.distmap[yind + self.y0][xind + self.x0] / self.discretization
         return 1000000
 
+    def get_map(self):
+        # return nav_msgs/OccupancyGrid of the contained buffer
+        pass
+"""
+    ================================================================================
+    MSG: nav_msgs/OccupancyGrid
+    # This represents a 2-D grid map, in which each cell represents the probability of
+    # occupancy.
+
+    Header header 
+
+    #MetaData for the map
+    MapMetaData info
+
+    # The map data, in row-major order, starting with (0,0).  Occupancy
+    # probabilities are in the range [0,1  Unknown is -1.
+    int8[] data
+
+    ================================================================================
+    MSG: std_msgs/Header
+    # Standard metadata for higher-level stamped data types.
+    # This is generally used to communicate timestamped data 
+    # in a particular coordinate frame.
+    # 
+    # sequence ID: consecutively increasing ID 
+    uint32 seq
+    #Two-integer timestamp that is expressed as:
+    # * stamp.secs: seconds (stamp_secs) since epoch
+    # * stamp.nsecs: nanoseconds since stamp_secs
+    # time-handling sugar is provided by the client library
+    time stamp
+    #Frame this data is associated with
+    # 0: no frame
+    # 1: global frame
+    string frame_id
+
+    ================================================================================
+    MSG: nav_msgs/MapMetaData
+    # This hold basic information about the characterists of the OccupancyGrid
+
+    # The time at which the map was loaded
+    time map_load_time
+    # The map resolution [m/cell]
+    float32 resolution
+    # Map width [cells]
+    uint32 width
+    # Map height [cells]
+    uint32 height
+    # The origin of the map [m, m, rad].  This is the real-world pose of the
+    # cell (0,0) in the map.
+    geometry_msgs/Pose origin
+    ================================================================================
+    MSG: geometry_msgs/Pose
+    # A representation of pose in free space, composed of postion and orientation. 
+    Point position
+    Quaternion orientation
+
+    ================================================================================
+    MSG: geometry_msgs/Point
+    # This contains the position of a point in free space
+    float64 x
+    float64 y
+    float64 z
+
+    ================================================================================
+    MSG: geometry_msgs/Quaternion
+    # This represents an orientation in free space in quaternion form.
+
+    float64 x
+    float64 y
+    float64 z
+    float64 w
+"""
 
 class LocalCostmap(object):
     """  Generates a costmap based off of sensor data from the car, without any global sense of map """
@@ -80,7 +154,6 @@ class LocalCostmap(object):
 
         laser_angles = np.linspace(data.angle_min, data.angle_max, math.ceil((data.angle_max - data.angle_min) / data.angle_increment))
         laser_ranges = data.ranges
-        
 
         laser_angles, laser_ranges  = self.filter_lasers(laser_angles, laser_ranges, data.range_min, data.range_max)
         laser_x, laser_y =  self.polar_to_euclid(laser_angles, laser_ranges)
@@ -93,16 +166,18 @@ class LocalCostmap(object):
 
         print ("Done computing distance map in:", time.clock() - start, "seconds")
 
-        # if self.visualize == True:
-        #     if self.im == None:
-        #         self.im = plt.imshow(self.buffer.distmap)
-        #         plt.colorbar(self.im)
-        #     else:
-        #         self.im.set_array(self.buffer.distmap)
-        #     plt.draw()
+        if self.visualize == True:
+            if self.im == None:
+                self.im = plt.imshow(self.buffer.distmap)
+                plt.colorbar(self.im)
+            else:
+                self.im.set_array(self.buffer.distmap)
+            plt.draw()
 
         self.first_laser_recieved = True  
 
+    def cost_at(self, x, y):
+        return 1.0 / self.buffer.dist_at(x, y)
 
 '''
     def generate_candidate_paths(self):
@@ -182,10 +257,11 @@ class PathEvaluator(object):
         pass
 
 class LocalExplorer(object):
-    def __init__(self):
+    def __init__(self, VISUALIZE=False):
         self.PLANNING_FREQ = 40
-
-        self.costmap = LocalCostmap()
+        self.VISUALIZE = VISUALIZE
+        
+        self.costmap = LocalCostmap(VISUALIZE)
         self.path_gen = PathGenerator()
         self.path_eval = PathEvaluator()
 
@@ -204,7 +280,7 @@ Path = collections.namedtuple("Path", ["steering_angle", "waypoints", "speed"])
 
 if __name__ == '__main__':
     try:
-        Localizer(False, True)
+        LocalExplorer()
     except rospy.ROSInterruptException:
         pass
     rospy.spin()
