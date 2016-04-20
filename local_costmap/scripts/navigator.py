88d515b8088a70e9ce28e1f8ef502d981bd4d308
Jessica Kenney <jessk@mit.edu>
2016-04-19 22:50:55 -0400

    Set goal to rightmost corridor

--- local_costmap/scripts/navigator.py
+++ local_costmap/scripts/navigator.py
@@ -3,16 +3,24 @@ import rospy
 from std_msgs.msg import Header, ColorRGBA
 from visualization_msgs.msg import Marker
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Pose, Vector3
+from geometry_msgs.msg import Pose, Point, Vector3, Quaternion
 import whoami
+import math
+import tf.transformations
+import numpy as np
 
 class Navigator(object):
 
-    STRAIGHT_AHEAD = [8, 0, 0]
+    STRAIGHT_AHEAD = [6, 0, 0]
 
     def __init__(self, visualize=False):
         self.should_visualize = visualize
         self.goal = self.STRAIGHT_AHEAD
+
+        # Each wall is a list of 2 points [[x0, y0], [x1, y1]]
+        self.walls = []
+        # Corridors are derived from walls (the spaces inbetween)
+        self.corridors = []
 
         self.laser_topic = "/scan" if whoami.is_racecar() else "/racecar/laser/scan"
         self.laser_sub = rospy.Subscriber(self.laser_topic, \
@@ -20,18 +28,63 @@ class Navigator(object):
 
         if self.should_visualize:
             self.marker_id = 0
-            self.viz_prefix = "local_costmap/navigator/viz/"
-            self.goal_viz_topic = self.viz_prefix + "goal"
-            self.corridor_viz_topic = self.viz_prefix + "corridors"
-            self.goal_viz_pub = rospy.Publisher(self.goal_viz_topic, Marker, queue_size=10)
-            self.corridor_viz_pub = rospy.Publisher(self.corridor_viz_topic, Marker, queue_size=10)
+            self.viz_pub = {}
+
+            def init_marker_publisher(name):
+                viz_prefix = "local_costmap/navigator/viz/"
+                topic = viz_prefix + name
+                self.viz_pub[name] = rospy.Publisher(topic, Marker, queue_size=10)
+
+            init_marker_publisher("goal")
+            init_marker_publisher("walls")
+            init_marker_publisher("corridors")
+
 
     def laser_update(self, laser_data):
-        """
-        Incorporates the laser data into its goal output
-        ( This method doesn't return a value )
-        """
-        pass
+        DISCONTINUITY_THRESHOLD = 1. # meters
+        INF = 1000.
+        ranges = np.array(laser_data.ranges)
+        # Set range_max points to be really far, to force a discontinuity
+        # (this might be only needed for the simulator)
+        ranges[ranges + 0.1 > laser_data.range_max] = INF
+        angles = (np.arange(ranges.shape[0]) * laser_data.angle_increment) + laser_data.angle_min
+        discontinuities = np.hstack(([False],
+                np.abs(ranges[:-1] - ranges[1:]) > DISCONTINUITY_THRESHOLD))
+        labeled = np.cumsum(discontinuities)
+        labels = np.unique(labeled)
+        new_walls = []
+        for label in labels:
+            indices = np.where(labeled == label)[0]
+            i_left = indices[0]
+            i_right = indices[-1]
+            if i_right - i_left < 5:
+                # Skip really short "walls"
+                continue
+            if ranges[i_left] == INF:
+                # Skip adding a "wall" when it's the edge of the vision range
+                continue
+            left_point  = self._polar_to_point(ranges[i_left],  angles[i_left])
+            right_point = self._polar_to_point(ranges[i_right], angles[i_right])
+            new_walls.append([left_point, right_point])
+        self.walls = new_walls
+        self.corridors = [[lw[1], rw[0]] for lw, rw in zip(self.walls, self.walls[1:])]
+        # TODO: the corridor is not best described by the endpoints of each wall,
+        # but rather the endpoint of one wall and the closest point on the other wall
+        # (whichever combo comes out shorter). This would capture the "doorway".
+
+        if len(self.corridors) > 0:
+            # Set goal to the rightmost corridor
+            [[x0, y0], [x1, y1]] = self.corridors[0] # [[x0, y0], [x1, y1]]
+            centerpoint_x = (x0 + x1)/2.
+            centerpoint_y = (y0 + y1)/2.
+            centerpoint_heading = math.atan2(y1 - y0, x1 - x0) - math.pi/2.
+            self.goal = [centerpoint_x, centerpoint_y, centerpoint_heading]
+        else:
+            self.goal = self.STRAIGHT_AHEAD
+
+    def _polar_to_point(self, distance, angle):
+        """ returns [x,y] """
+        return [distance*math.cos(angle), distance*math.sin(angle)]
 
     def camera_update(self, camera_data):
         """
@@ -44,29 +97,52 @@ class Navigator(object):
         """
         Returns the current goal point as a list [x, y, heading] in the base frame.
         """
-        return self.goal
+        return self.goal[:]
 
     def visualize(self):
         """
-        Output a marker for the goal point
+        Output a marker for the goal point, and markers for the walls & corridors
         """
         if not self.should_visualize:
             return
+        self.viz_pub["goal"].publish(self._make_pose_marker(self.goal, ColorRGBA(0,1,1,1)))
+        self.viz_pub["walls"].publish(self._make_segment_markers(self.walls, ColorRGBA(1,0,1,1)))
+        self.viz_pub["corridors"].publish(self._make_segment_markers(self.corridors, ColorRGBA(0,1,0,1)))
 
-        goal = Marker()
-        goal.header = Header(
+    def _init_marker(self):
+        x = Marker()
+        x.header = Header(
                 stamp=rospy.Time.now(),
-                frame_id="hokuyo_link") #TODO this should be base someday
-        goal.ns = "navigator"
-        goal.id = self.marker_id
+                frame_id="hokuyo_link")
+        x.ns = "navigator"
+        x.id = self.marker_id
         self.marker_id += 1
+        x.action = 0
+        x.lifetime = rospy.Duration.from_sec(10.)
+        x.points = []
+        return x
+
+    def _make_pose_marker(self, pose, color):
+        """ pose is [x,y,heading] """
+        goal = self._init_marker()
         goal.type = Marker.ARROW
-        goal.action = 0
-        goal.color = ColorRGBA(0, 1, 1, 1)
-        goal.lifetime = rospy.Duration.from_sec(10.)
+        goal.color = color
         goal.pose = Pose()
         goal.pose.position.z = 0.
-        goal.pose.position.x = self.goal[0]
-        goal.pose.position.y = self.goal[1]
+        goal.pose.position.x = pose[0]
+        goal.pose.position.y = pose[1]
+        goal.pose.orientation = Quaternion(*tf.transformations.quaternion_from_euler(0,0,pose[2]))
         goal.scale = Vector3(.5, .1, .1)
-        self.goal_viz_pub.publish(goal)
+        return goal
+
+    def _make_segment_markers(self, segments, color):
+        walls = self._init_marker()
+        walls.type = Marker.LINE_LIST
+        walls.color = color
+        for wall in segments:
+            walls.points.append(Point(wall[0][0], wall[0][1], 0))
+            walls.points.append(Point(wall[1][0], wall[1][1], 0))
+        walls.scale.x = .1
+        return walls
+
+
